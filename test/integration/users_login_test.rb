2c3f460aadbe30dd560340c83920ffb29923f02a
Loconav <loconav@Loconavs-MacBook-Pro.local>
2021-06-03 12:43:27 +0530

    Implement advanced login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -33,4 +33,16 @@ assert_select "a[href=?]", login_path
 assert_select "a[href=?]", logout_path, count: 0
 assert_select "a[href=?]", user_path(@user), count: 0
 end
+
+test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_equal FILL_IN, assigns(:user).FILL_IN
+    end
+    test "login without remembering" do
+    # Log in to set the cookie.
+    log_in_as(@user, remember_me: '1')
+    # Log in again and verify that the cookie is deleted.
+    log_in_as(@user, remember_me: '0')
+    assert cookies[:remember_token].blank?
+end
 end
